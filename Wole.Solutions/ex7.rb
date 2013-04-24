76c38dc67f80eeee939b0dfeefa43db813eea43e
Wole Solana <osolana@thoughtworks.com>
2013-04-24 20:09:08 +0200

    Ex 7, 8 and 9 from Wole

---
+++ Wole.Solutions/ex7.rb
@@ -0,0 +1,27 @@
+# Print the following block of code
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+puts "." * 10 # prints the '.' character 10 times
+
+# Assign the letters of the word 'CheeseBurger' to different variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print instead of puts here. Change it to puts and # see what happens
+# Using 'print' prints the characters together without spaces or line breaks
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# this just is polite use of the terminal, try removing it
+puts
