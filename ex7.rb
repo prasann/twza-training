54dd26b6f7cbde2749f1bac8d4c35f0413ab1468
Musa Baloyi <musabaloyi@thoughtworks.com>
2013-04-24 16:49:33 +0200

    =Commit 7 - 10

---
+++ ex7.rb
@@ -0,0 +1,23 @@
+puts "Mary had a little lamb." # print first line of poem
+puts "Its fleece was as white as %s." % 'snow' # print the second line of poem and treat 'snow' the value within a string variable
+puts "And everywhere that Mary went." # print third line of poem
+puts "."*10	# print '.' 10 times
+
+# assign each character within CheeseBurger to a variable
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1+end2+end3+end4+end5+end6 # print the first 6 characters
+print end7+end8+end9+end10+end11+end12 # print the nextx 6 characters
+
+puts # print a space
