6232bdf8cfb3b43a900adf7a4aff389de778bd9e
Wole Solana <osolana@thoughtworks.com>
2013-04-30 17:24:47 +0200

    Exercise 33

---
+++ Wole.Solutions/ex33.rb
@@ -0,0 +1,23 @@
+# Modified code turning the while loop into a function
+def array_create(size, step_size)
+	i = 0
+	numbers = []
+
+	while i < size
+		puts "At the top i is #{i}"
+		numbers.push(i)
+
+		i = i + 1 
+		puts "Numbers now: #{numbers}"
+		puts "At the bottom i is #{i}"
+	end
+
+	puts "The numbers: "
+	for num in numbers
+		puts num
+	end
+end
+
+size = 8
+step_size = 2
+array_create(size, step_size)
