3cf591a0acf9d7989aafa085cde998bb90b953b7
Wole Solana <osolana@thoughtworks.com>
2013-05-02 09:41:51 +0200

    Exercise 33 (edited) - 35

--- Wole.Solutions/ex33.rb
+++ Wole.Solutions/ex33.rb
@@ -18,6 +18,24 @@ def array_create(size, step_size)
 	end
 end
 
+
+# Same function using a 'for' loop
+def make_array(size)
+	numbers = []
+	for i in (0..size)
+		puts "At the top, i is #{i}"
+		numbers.push(i)
+		puts "Numbers now: #{numbers}"
+		puts "At the bottom, i is #{i}"
+	end
+
+	puts "The numbers: "
+	for num in numbers
+		puts num
+	end
+end
+
 size = 8
 step_size = 2
-array_create(size, step_size)
+#array_create(size, step_size)
+make_array(size)
---
+++ Wole.Solutions/ex34.rb
@@ -0,0 +1,6 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts animals.first
+puts animals.last
+
+puts "The animal in position 0 is: #{animals[0]}"
